4596c83d5d752c7c8c83c2a4d38af851546f205b
soooru <threequaters9@gmail.com>
2023-04-16 16:42:13 +0900

    init: 프로젝트 세팅, 헤더 추가

---
+++ src/components/common/Header.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Link from 'next/link';
+import styles from '@/styles/header.module.scss';
+import Image from 'next/image';
+
+interface Props {
+  rightElements?: React.ReactElement[];
+}
+
+const HeaderComponent = ({ rightElements }: Props) => {
+  return (
+    <header className={styles.header}>
+      <div className={styles.flexItem}>
+        <Link href="/" className={styles.box}>
+          <Image
+            src="https://lecture-1.vercel.app/inflearn.png"
+            width={110}
+            height={20}
+            alt="인프런 로고"
+          />
+        </Link>
+      </div>
+      {rightElements && <div className={styles.flexItem}>{rightElements}</div>}
+    </header>
+  );
+};
+
+export default HeaderComponent;
---
+++ src/pages/feedback.tsx
@@ -0,0 +1,12 @@
+import { Fragment } from 'react';
+import Header from '@/components/common/Header';
+
+export default function Home() {
+  return (
+    <>
+      <Fragment>
+        <Header></Header>
+      </Fragment>
+    </>
+  );
+}
