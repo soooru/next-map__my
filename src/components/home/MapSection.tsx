4ce5da2d15fe9248eb7169f6b16858c5eca1e8df
soooru <threequaters9@gmail.com>
2023-04-23 16:40:49 +0900

    feat:지도 마커에 클릭 이벤트 추가

--- src/components/home/MapSection.tsx
+++ src/components/home/MapSection.tsx
@@ -1,12 +1,16 @@
 import Map from './Map';
 import Markers from './Markers';
 import useMap from '../../hooks/useMap';
+import useCurrentStore from '@/hooks/useCurrentStore';
 import type { NaverMap } from '../../types/map';
 
 const MapSection = () => {
   const { initializeMap } = useMap();
+  const { clearCurrentStore } = useCurrentStore();
+
   const onLoadMap = (map: NaverMap) => {
     initializeMap(map);
+    naver.maps.Event.addListener(map, 'click', clearCurrentStore);
   };
 
   return (
